8ea63212e083b8245e9ba0d7ebaac9dff0b8dd32
Jonas Paulsson <paulson1@linux.ibm.com>
2025-03-21 14:30:08 -0600

    Revert "Disable pre-ra scheduling."
    This reverts commit 05b5f4cf28eae4d4215b1eea8725ca221da45d68.

--- llvm/lib/CodeGen/MachineScheduler.cpp
+++ llvm/lib/CodeGen/MachineScheduler.cpp
@@ -550,7 +550,6 @@ bool MachineSchedulerLegacy::runOnMachineFunction(MachineFunction &MF) {
   if (skipFunction(MF.getFunction()))
     return false;
 
-return false;
   if (EnableMachineSched.getNumOccurrences()) {
     if (!EnableMachineSched)
       return false;
@@ -584,8 +583,6 @@ PostMachineSchedulerPass::~PostMachineSchedulerPass() = default;
 PreservedAnalyses
 MachineSchedulerPass::run(MachineFunction &MF,
                           MachineFunctionAnalysisManager &MFAM) {
-return PreservedAnalyses::all();
-
   if (EnableMachineSched.getNumOccurrences()) {
     if (!EnableMachineSched)
       return PreservedAnalyses::all();
